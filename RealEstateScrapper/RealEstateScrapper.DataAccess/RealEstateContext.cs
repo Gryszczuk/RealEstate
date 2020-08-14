2bf7d4ffac70bca01c0177679b84e031c1cde6e5
Gryszczuk <pangryszczuk@gmail.com>
2020-08-14 16:13:40 +0200

    added dbcontext

---
+++ RealEstateScrapper/RealEstateScrapper.DataAccess/RealEstateContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RealEstateScrapper.Models;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace RealEstateScrapper.DataAccess
+{
+   public class RealEstateContext : DbContext
+    {
+        public RealEstateContext(DbContextOptions<RealEstateContext> options) 
+            : base(options)
+        {
+        }
+        public DbSet<Offer> Offers { get; set; }
+    }
+}
