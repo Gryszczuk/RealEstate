165a84d891920092a70f5e62404d5f03e18b3ce9
Gryszczuk <pangryszczuk@gmail.com>
2020-08-14 13:59:11 +0200

    offer model added

---
+++ RealEstateScrapper/RealEstateScrapper.Models/BaseModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealEstateScrapper.Models
+{
+   public class BaseModel
+    {
+        public string Id { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
